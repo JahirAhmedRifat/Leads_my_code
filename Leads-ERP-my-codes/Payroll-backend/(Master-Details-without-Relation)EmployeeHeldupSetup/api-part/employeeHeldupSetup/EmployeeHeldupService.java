e0a80f973801e253c23652c2bdc140c3ee25dbee
JahirAhmedRifat <mdjahir101096@gmail.com>
2026-02-10 00:03:18 +0600

    1

--- Leads-ERP-my-codes/Payroll-backend/(Master-Details-without-Relation)EmployeeHeldupSetup/api-part/employeeHeldupSetup/EmployeeHeldupService.java
+++
@@ -1,9 +0,0 @@
-package com.leads.microcube.payroll.employeeHeldupSetup;
-
-import com.leads.microcube.payroll.employeeHeldupSetup.command.EmployeeHeldupRequest;
-import com.leads.microcube.payroll.employeeHeldupSetup.query.EmployeeHeldupResponse;
-
-public interface EmployeeHeldupService {
-    EmployeeHeldupResponse heldUpCreate(EmployeeHeldupRequest request);
-    EmployeeHeldupResponse updateHeldup(EmployeeHeldupRequest request);
-}
